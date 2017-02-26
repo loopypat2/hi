763abc01be7cb5450811cf30fa93630041cb00cd
loopypat2 <lickmypeach2@gmail.com>
2017-02-26 18:13:49 +0000

    Add files via upload

--- task1.sh
+++ task1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #  Start date (without time)
-start_date=`head -1 example.log | cut -f3`
+start_date=`head -1 example.log | cut -f3 | cut -c2-12 `
 
 #  End date and time
-end_date=`tail -1 example.log | cut -f3`
+end_date=`tail -1 example.log | cut -f3 | cut -c2-21 `
 
 #  Print the output
 printf "$start_date to $end_date"
